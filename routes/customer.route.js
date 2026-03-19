4e312a3d52884d20018a36f87b1035399d9b41f5
Stephanie <stephaniecharles147@gmail.com>
2026-03-19 16:51:08 +0100

    feat: add customer settings endpoints for profile update, password change, and account deletion

--- routes/customer.route.js
+++ routes/customer.route.js
@@ -3,11 +3,14 @@ import express from "express";
 import Admin from "../models/admin.model.js";
 
 import { verifyAccessByModel, verifyAccessByLogin } from "../middlewares/verification.js";
-import { getAllCustomer, getCustomerById } from "../controllers/customer.controller.js";
+import { getAllCustomer, getCustomerById, updateProfile, changePassword, deleteAccount } from "../controllers/customer.controller.js";
 
 const router = express.Router();
 
 router.get("/", verifyAccessByModel(Admin), getAllCustomer)
 router.get("/:id", verifyAccessByLogin, getCustomerById)
+router.patch("/settings/profile", verifyAccessByModel(Customer), updateProfile)
+router.patch("/settings/change-password", verifyAccessByModel(Customer), changePassword)
+router.delete("/settings/delete-account", verifyAccessByModel(Customer), deleteAccount)
 
 export default router;
